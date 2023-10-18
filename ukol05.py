83479db853cd5b277d18ec47993789a8ae10bcd6
marketat <145936326+marketat@users.noreply.github.com>
2023-10-19 00:39:40 +0200

    Ukol05

---
+++ ukol05.py
@@ -0,0 +1,60 @@
+teploty = [
+    [2.1, 5.2, 6.1, -0.1],
+    [2.2, 4.8, 5.6, -1.0],
+    [3.3, 6.5, 5.9, 1.2],
+    [2.9, 5.6, 6.0, 0.0],
+    [2.0, 4.6, 5.5, -1.2],
+    [1.0, 3.2, 2.1, -2.0],
+    [0.4, 2.7, 1.3, -2.8]
+]
+
+#průměrné teploty
+import statistics
+
+prumer = [statistics.mean(teplota) for teplota in teploty]
+print(prumer)
+
+#ranní teploty
+ranni_t = [rano[0] for rano in teploty]
+print(ranni_t)
+
+#noční teploty
+nocni_t = [noc[3] for noc in teploty]
+print(nocni_t)
+
+#polední a noční teploty
+dve_teploty = [[teplota[1], teplota[3]] for teplota in teploty]
+print(dve_teploty)
+
+#bonus 
+teploty = [
+    ["pondělí", 2.1, 5.2, 6.1, -0.1],
+    ["úterý", 2.2, 4.8, 5.6, -1.0],
+    ["středa", 3.3, 6.5, 5.9, 1.2],
+    ["čtvrtek", 2.9, 5.6, 6.0, 0.0],
+    ["pátek", 2.0, 4.6, 5.5, -1.2],
+    ["sobota", 1.0, 3.2, 2.1, -2.0],
+    ["neděle", 0.4, 2.7, 1.3, -2.8]
+]
+
+
+d = [d[0] for d in teploty]
+print(d)
+
+
+t = [t[1:4] for t in teploty]
+print(t)
+
+prumer = {}
+
+""""prumer = {} for d, t in teploty:
+    prumer[d] = t
+
+prumer = dict((d, t) for d, t in teploty
+prumer = {den: teplota for den, teplota in enumerate(teploty)}
+prumer = {f"(hodnota[0])": statistics.mean(hodnota[1:4]) for hodnota[0], hodnota in teploty}
+
+prumer = {d: statistics.mean(t) for d, t in teploty}
+"""
+
+print(prumer)
